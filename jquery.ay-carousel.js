35290c947cf7fd8e8102f3a74727d589d31e5003
Gajus Kuizinas <g.kuizinas@anuary.com>
2012-11-09 12:25:07 +0000

    Added support to identify the disabled navigation.

--- jquery.ay-carousel.js
+++ jquery.ay-carousel.js
@@ -1,5 +1,5 @@
 /**
- * jQuery carousel v0.0.1 (2012 NOV 8)
+ * jQuery carousel v0.0.2 (2012 NOV 9)
  * https://github.com/gajus/ay-carousel
  *
  * Licensed under the BSD.
@@ -15,7 +15,9 @@
 			var carousel = $(this[i]),
 				navigation = carousel.find('.button-left, .button-right'),
 				body = carousel.find('.body'),
-				wrapper = body.find('.wrapper')
+				wrapper = body.find('.wrapper'),
+				body_width	= body.width(),
+				wrapper_width = wrapper.width(),
 				elements = wrapper.children(),
 				wrapper_width = 0,
 				margin_left = 0;
@@ -27,19 +29,15 @@
 			
 			wrapper.css({width: wrapper_width});
 			
-			//console.log(  );
-			
 			var update_navigation	= function(){
 				$(navigation[0])[margin_left === 0 ? 'addClass' : 'removeClass']('disabled');
-				//$(navigation[0])[margin_left === 0 ? 'addClass' : 'removeClass']('disabled');
+				$(navigation[1])[wrapper_width-body_width+margin_left === 0 ? 'addClass' : 'removeClass']('disabled');
 			};
 			
 			update_navigation();
 			
 			navigation.on('click', function(){
-				var body_width	= body.width(),
-					wrapper_width = wrapper.width(),
-					wrapper_offset = parseInt(wrapper.css('margin-left'));
+				var wrapper_offset = parseInt(wrapper.css('margin-left'));
 					request_direction = $(this).hasClass('button-left') ? 0 : 1,
 					remaining_right = wrapper_width+wrapper_offset-body_width,
 					remaining_left = wrapper_offset*-1;
